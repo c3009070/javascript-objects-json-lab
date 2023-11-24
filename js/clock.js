1fd140a2f83d9728b2cd33652f247dccbdaf239a
Alliaj <c3009072@hallam.shu.ac.uk>
2023-11-24 12:06:17 +0000

    gy

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+(function(){ 
+console.info("Hello JS"); 
+const myNode = document.createElement("div");
+const digitalclock = document.getElementById("digitalClock")
+digitalclock.appendChild(Mynode);
+
+const myDate = new date()
+console.dir(myDate);
+myNode.innerHTML = myDate.tolocateTimesring();
+
+})();
+
